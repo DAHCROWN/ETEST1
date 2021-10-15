b3776ab2e970766c6ed752cea5458ed510424820
DAHCROWN <dsilakass@gmail.com>
2021-10-15 19:00:41 +0100

    git commit -m "friday"

--- env/app.py
+++ env/app.py
@@ -1,49 +1,44 @@
 from flask import Flask,render_template,jsonify,request, redirect, url_for, session
 from examination.exam1 import exam_list
+import flask
+from flask.helpers import make_response
 from werkzeug.utils import redirect
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token, get_jwt_identity
-)
+from werkzeug.wrappers import response
 
 app = Flask(__name__)
-jwt = JWTManager(app)
 app.config['SECRET_KEY'] = 'random'
 app.config['JWT_SECRET_KEY'] = "b'j\x98\xcc\x088\xe9m3\xe1\xde&\x850\xaf\xcb\x19"
-app.config['JWT_HEADER_NAME'] = "Authorization"
-app.config['JWT_HEADER_TYPE'] = "Bearer"
-app.config['JWT_TOKEN_LOCATION'] = ['headers', 'query_string']
 
-
-
+def logged_in():
+        return redirect(url_for('index'))
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    headers = request.headers
+    session['username'] = ''
     if request.method == 'POST':
         username = request.form['username']
         otp = request.form['otp']
         session['username'] = request.form['username']
         if request.form['otp'] == 'test':
-            session['username'] = request.form['username']
-            access_token = create_access_token(identity={"user_id": username})
-            return redirect(url_for('courses')), {"access_token": access_token}
+            session['username'] = username
+            return redirect(url_for('courses'))
         else:
             return render_template('index.html', error = "PLEASE ENTER VALID LOGIN DETAILS")
             
     return render_template('index.html')
 
 @app.route('/courses')
-@jwt_required()
 def courses():
-    user = get_jwt_identity()
-    return render_template('course_pg.html', user=user)
+    if session['username'] == '':
+        return redirect(url_for('index'))       
+    else:
+        return render_template('course_pg.html')
 
 @app.route('/admin')
 def forbid():
     redirect(url_for('index'))
 
 @app.route('/instructions/<string:exam_id>', methods =['POST', 'GET'])
-@jwt_required()
 def instructions(exam_id):
 
     exam_ids= {'1': 'MAT111',
@@ -61,13 +56,16 @@ def instructions(exam_id):
 
 
 @app.route('/exams/<string:exam_id>')
-@jwt_required()
 def exam(exam_id):
     return render_template('test.html', exam_id=exam_id)
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+    
 from examination import exam1
 app.register_blueprint(exam1.bp)
 
 if "__name__" == "__main__":
     app.run(debug=True)
-
